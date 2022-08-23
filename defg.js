58373412a4d7fe88af94429ade73710e8f5db4f2
Charles L <charles.lobo@gmail.com>
2022-08-23 15:30:11 +1000

    feat: update documentation

--- defg.js
+++ defg.js
@@ -1,32 +1,45 @@
 #!/usr/bin/env node
 //** # DEFG - Generate documentation from code comments
 //**
-//** ## WHY!?
+//** ## Motivation
 //**
 //** I find that README's and other documentations tend to get out of
-//** date really _really_ quickly. The hope here is that user documentation
-//** kept close to code should be easier to access, modify, and update.
+//** date quickly. Using `defg` you can simply generate your README
+//** from documentation comments, and as these comments are close
+//** to the code, they are be easier to access, modify, and update.
+//**
+//** It still takes some discipline to update - instead of ignoring -
+//** the documentation comments, but it's easier to do as you are
+//** changing/reviewing the code.
 //**
 //** ## How does it work?
 //**
 //** `defg` trawls through all 'programming' files (.js, .sh, .java, .c, .cpp,...)
-//** it finds and extracts 'special' comments that start with `//**` or `##**` -
-//** these are considered 'user documentation' comments. It then checks that all these
-//** lines are present in the README. If they are, it generates a nice PDF from
-//** and opens it.
+//** it finds and extracts 'special' comments that start with `//**` or `##**`.
+//** These are considered 'user documentation' comments. It then uses them to
+//** update the README, generates a nice PDF, and opens it.
 //**
 //** ## Why not just generate the PDF from the code documentation?
 //**
-//** Because when we write documentation in code file it's probably related to the
+//** Because when we write documentation in code file it should be related to the
 //** code in the file. This documentation may be distributed across multiple
-//** files.
+//** files - close to classes and functions.
 //**
 //** The actual user documentation will have (a) a nice, defined, flow (ordering
-//** of the documentation pieces) and (b) additional information that may not
+//** of the documentation pieces) and (b) additional images, html etc, that may not
 //** quite fit into a specific code file.
 //**
-//** All in all, having the documentation updated separately turns out to be better
-//** than trying to auto-generate it.
+//** ## First Run
+//**
+//** If you run `defg` and there is no README.md file, it will generate one from
+//** all the documentation it has found. During this process, it can get the order
+//** of comments all mixed up. You are encouraged to then go and reorder all the
+//** pieces in the README.md to get it into a nice shape.
+//**
+//** ## Adding Images
+//**
+//** In order to make your documentation prettier, you can chose to include images
+//** etc. If you do this, `defg` will attempt to preserve these in the updated README.
 //**
 //** ## Usage
 //** ```
